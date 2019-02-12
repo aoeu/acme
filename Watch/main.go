9da6a50ea11a68c2128cbf49924e4e1147b69056
Travis McDemus <678571+aoeu@users.noreply.github.com>
2019-02-12 02:09:22 -0500

    Kludge cmd into working cross-platform

--- Watch/main.go
+++ Watch/main.go
@@ -2,11 +2,23 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// This file was originally taken from:
+// https://github.com/rsc/rsc/blob/e1cb635eaa1bed9efc1ec21c8537c46161abfcb8/cmd/Watch/main.go
+//
+// This code has been modified due to the problem that this program used syscalls specific to macOS,
+// as explained by bradfitz:
+// 	https://groups.google.com/forum/#!msg/golang-dev/x18NTIq4g6A/zM1i76ikBAAJ
+//
+
 // Watch runs a command each time files in the current directory change.
 //
 // Usage:
 //
 //	Watch cmd [args...]
+//
+// Example:
+//
+// Watch wc -l main.go
 //
 // Watch opens a new acme window named for the current directory
 // with a suffix of /+watch. The window shows the execution of the given
@@ -22,41 +34,54 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"9fans.net/go/acme"
+	"github.com/rjeczalik/notify"
 )
 
 var args []string
 var win *acme.Win
 var needrun = make(chan bool, 1)
 
-var kq struct {
+// TODO(aoeu): Rename struct to something more relevant now that Kqueue syscall usage is removed.
+var watchList struct {
 	fd   int
 	dir  *os.File
 	m    map[string]*os.File
 	name map[int]string
 }
 
-func kadd(fd int) {
-	kbuf := make([]syscall.Kevent_t, 1)
-	kbuf[0] = syscall.Kevent_t{
-		Ident:  uint64(fd),
-		Filter: syscall.EVFILT_VNODE,
-		Flags:  syscall.EV_ADD | syscall.EV_RECEIPT | syscall.EV_ONESHOT,
-		Fflags: syscall.NOTE_DELETE | syscall.NOTE_EXTEND | syscall.NOTE_WRITE,
+// watch watches changes made on file-system by the text editor
+// when saving a file. Usually, either InCloseWrite or InMovedTo (when swapping
+// with a temporary file) event is created, so only those events are monitored.
+// This function is lifted from https://github.com/rjeczalik/notify/blob/master/example_inotify_test.go#L20
+func watch(path string) {
+	// Make the channel buffered to ensure no event is dropped. Notify will drop
+	// an event if the receiver is not able to keep up the sending pace.
+	c := make(chan notify.EventInfo, 1)
+
+	// Set up a watchpoint listening for inotify-specific events within a
+	// current working directory. Dispatch each InCloseWrite and InMovedTo
+	// events separately to c.
+	//
+	// func Watch(path string, c chan<- EventInfo, events ...Event) error
+	//
+	if err := notify.Watch(path, c, notify.InCloseWrite, notify.InMovedTo); err != nil {
+		log.Fatal(err)
 	}
-	n, err := syscall.Kevent(kq.fd, kbuf[:1], kbuf[:1], nil)
-	if err != nil {
-		log.Fatalf("kevent: %v", err)
-	}
-	ev := &kbuf[0]
-	if n != 1 || (ev.Flags&syscall.EV_ERROR) == 0 || int(ev.Ident) != int(fd) || int(ev.Filter) != syscall.EVFILT_VNODE {
-		log.Fatal("kqueue phase error")
-	}
-	if ev.Data != 0 {
-		log.Fatalf("kevent: kqueue error %s", syscall.Errno(ev.Data))
+	defer notify.Stop(c)
+
+	for {
+		// Block until an event is received.
+		switch ei := <-c; ei.Event() {
+		case notify.InCloseWrite:
+			// log.Println("Editing of", ei.Path(), "file is done.")
+			needrun <- true
+		case notify.InMovedTo:
+			// log.Println("File", ei.Path(), "was swapped/moved into the watched directory.")
+			needrun <- true
+		}
 	}
 }
 
@@ -86,69 +111,40 @@ func main() {
 	go events()
 	go runner()
 
-	kq.fd, err = syscall.Kqueue()
-	if err != nil {
-		log.Fatal(err)
-	}
-	kq.m = make(map[string]*os.File)
-	kq.name = make(map[int]string)
+	watchList.fd = 777
+	watchList.m = make(map[string]*os.File)
+	watchList.name = make(map[int]string)
 
 	dir, err := os.Open(".")
 	if err != nil {
 		log.Fatal(err)
 	}
-	kq.dir = dir
-	kadd(int(dir.Fd()))
+	watchList.dir = dir
+	watch(".")
 	readdir := true
 
 	for {
 		if readdir {
-			kq.dir.Seek(0, 0)
-			names, err := kq.dir.Readdirnames(-1)
+			watchList.dir.Seek(0, 0)
+			names, err := watchList.dir.Readdirnames(-1)
 			if err != nil {
 				log.Fatalf("readdir: %v", err)
 			}
 			for _, name := range names {
-				if kq.m[name] != nil {
+				if watchList.m[name] != nil {
 					continue
 				}
 				f, err := os.Open(name)
 				if err != nil {
 					continue
 				}
-				kq.m[name] = f
+				watchList.m[name] = f
 				fd := int(f.Fd())
-				kq.name[fd] = name
-				kadd(fd)
+				watchList.name[fd] = name
+				watch(name)
 			}
 		}
-
-		kbuf := make([]syscall.Kevent_t, 1)
-		var n int
-		for {
-			n, err = syscall.Kevent(kq.fd, nil, kbuf[:1], nil)
-			if err == syscall.EINTR {
-				continue
-			}
-			break
-		}
-		if err != nil {
-			log.Fatalf("kevent wait: %v", err)
-		}
-		ev := &kbuf[0]
-		if n != 1 || int(ev.Filter) != syscall.EVFILT_VNODE {
-			log.Fatal("kqueue phase error")
-		}
-
-		select {
-		case needrun <- true:
-		default:
-		}
-
-		fd := int(ev.Ident)
-		readdir = fd == int(kq.dir.Fd())
 		time.Sleep(100 * time.Millisecond)
-		kadd(fd)
 	}
 }
 
